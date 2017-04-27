8558fabadc3db0820ee62d6b135c935cccf902c0
cgpuglie <cgpuglie@asu.edu>
2017-04-26 18:39:50 -0700

    Logging improvements to auth route - Ability to add config yaml file

--- src/homebrew-monitor-auth/index.js
+++ src/homebrew-monitor-auth/index.js
@@ -1,30 +1,44 @@
-const express = require('express');
+const express = require('express')
 const morgan = require('morgan')
+const chalk = require('chalk')
+const yaml = require('js-yaml')
+const fs = require('fs')
+const path = require('path')
 const { json: jsonParser } = require('body-parser-json')
 
+// load ENV
 const {
 	SERVICE_BIND_IP='0.0.0.0',
 	SERVICE_PORT=8081,
 	SERVICE_ROOT='/',
+	SERVICE_COLOR=true,
 	
 	BREW_MASTER='admin',
 	BREW_MASTER_PASS='password'
 } = process.env
 
+// load service config
+const {
+	name='service',
+	color,
+} = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'config.yml')))
+
 const app = express()
 
-app.use(morgan('tiny'))
+app.use(morgan(`${ !color ? name : chalk[color](name)} > :method :status :url - :response-time ms`))
 app.use(jsonParser())
 
 // set base root
 app.all(SERVICE_ROOT)
 // set health check route
 app.get(
-	'/',
+	'/health',
 	function health (req, res) {
-		return res.send({
-			ok: true
-		})
+		return res
+			.status(200)
+			.send({
+				ok: true
+			})
 	}
 )
 app.post(
@@ -40,5 +54,5 @@ app.post(
 const server = app.listen(SERVICE_PORT, SERVICE_BIND_IP, () => {
   const {address, port} = server.address();
 
-  console.log(`Homebrew-monitor-auth listening at http://${address}:${port}`);
+  console.log(`${!color ? name : chalk[color](name) } > listening at http://${address}:${port}`);
 });
