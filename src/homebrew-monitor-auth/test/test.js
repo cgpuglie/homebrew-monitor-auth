fa2935ee18a78f8af0435350a47153e9652cfea4
cgpuglie <cgpuglie@asu.edu>
2017-04-30 18:41:40 -0700

    Added test for desired auth state with JWT - Failing - needs implementation

--- src/homebrew-monitor-auth/test/test.js
+++ src/homebrew-monitor-auth/test/test.js
@@ -2,8 +2,19 @@ const should = require('should')
 const rp = require('request-promise')
 
 // start app server
-const { service, config: {port, root} } = require('../index')
+const { service, config: {port, root, master: username, pass: password} } = require('../index')
 const base = `http://localhost:${port}${root}`
+
+// state object enclosing properties to override test closures
+let state = {}
+// set property, can be passed to promise.then
+const setProp = (name) =>
+  (input) => {
+    state[name] = input
+    return input
+  }
+// function wrapper to delay evaluation
+const getProp = (name) => state[name]
 
 describe('server', function server() {
   it('Should start', function startServer() {    
@@ -22,5 +33,28 @@ describe('/health', function describeHealth() {
     .then(res => res.statusCode)
     .should.eventually.equal(200)
     
+  })
+})
+
+describe('login', function describeLogin() {
+  it('Should return a json webtoken', function tokenCheck () {
+    return rp({
+      uri: `${base}/`,
+      method: 'POST',
+      json: {username, password}
+    })
+    .then(body => body.token)
+    .then(setProp('jwt'))
+    .should.eventually.not.equal(undefined)
+  })
+
+  it('Should validate a token', function tokenValidate () {
+    return rp({
+      uri: `${base}/verify`,
+      method: 'POST',
+      json: { token: getProp('jwt') }
+    })
+    .then(body => body.user)
+    .should.eventually.equal(username)
   })
 })
